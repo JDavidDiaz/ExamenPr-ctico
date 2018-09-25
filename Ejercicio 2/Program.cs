89bd05c133c3bf8a707f71dbcda972193aab8c2d
JDavidDiaz <43283432+JDavidDiaz@users.noreply.github.com>
2018-09-24 19:46:26 -0700

    Ejercicio2

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Cantidad;
+            Console.WriteLine("Ingrese la cantidad de numeros para la sucesion");
+            Cantidad = int.Parse(Console.ReadLine());
+            int[] Sucesion = new int[Cantidad];
+            for (int i = 0; i < Cantidad; i++)
+            {
+                Console.WriteLine("ingrese el {0} numero de la sucesion", i + 1);
+                Sucesion[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Reverse(Sucesion);
+            Console.WriteLine("\nSucesion invertida\n");
+            for (int j = 0; j < Cantidad; j++)
+            {
+                Console.WriteLine("{0}", Sucesion[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
