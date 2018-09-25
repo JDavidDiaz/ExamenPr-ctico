c857c510814d5c51ee4359dd43c8ceda81063d74
JDavidDiaz <43283432+JDavidDiaz@users.noreply.github.com>
2018-09-24 19:47:08 -0700

    Ejercicio3

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Cantidad;
+            Console.WriteLine("Ingrese la cantidad de numeros para la sucesion");
+            Cantidad = int.Parse(Console.ReadLine());
+            int[] Sucesion = new int[Cantidad];
+            for (int i = 0; i < Cantidad; i++)
+            {
+                Console.WriteLine("ingrese el {0} numero", i + 1);
+                Sucesion[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nEl ultimo elemento con mayor valor es: {0}, en la posicion {1}", Sucesion.Max(), Sucesion.ToList().LastIndexOf(Sucesion.Max()) + 1);
+            Console.ReadKey();
+        }
+    }
+
+}
