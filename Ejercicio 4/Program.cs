7e1e9807e34ff016843f4b92793a61df000f717c
JDavidDiaz <43283432+JDavidDiaz@users.noreply.github.com>
2018-09-24 19:48:43 -0700

    Ejercicio4

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Sucesion = new int[3];
+            Console.WriteLine("Ingresa 3 numeros");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("\nnumero {0}:  ", i + 1);
+                Sucesion[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nEl elemento menor es {0}", Sucesion.Min());
+            Console.ReadKey();
+        }
+    }
+
+}
